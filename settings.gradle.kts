0d330ed01ec63d2debf2f8111ecaba3cf1e3f9cf
Adam <897017+aSemy@users.noreply.github.com>
2024-09-29 10:28:26 +0200

    tidy repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,31 +15,22 @@ dependencyResolutionManagement {
 		mavenCentral()
 
 		// Declare the Node.js & Yarn download repositories
-		exclusiveContent {
-			forRepository {
-				ivy("https://nodejs.org/dist/") {
-					name = "Node Distributions at $url"
-					patternLayout { artifact("v[revision]/[artifact](-v[revision]-[classifier]).[ext]") }
-					metadataSources { artifact() }
-					content { includeModule("org.nodejs", "node") }
-				}
-			}
-			filter { includeGroup("org.nodejs") }
+		ivy("https://nodejs.org/dist/") {
+			name = "Node Distributions at $url"
+			patternLayout { artifact("v[revision]/[artifact](-v[revision]-[classifier]).[ext]") }
+			metadataSources { artifact() }
+			content { includeModule("org.nodejs", "node") }
 		}
 
-		exclusiveContent {
-			forRepository {
-				ivy("https://github.com/yarnpkg/yarn/releases/download") {
-					name = "Yarn Distributions at $url"
-					patternLayout { artifact("v[revision]/[artifact](-v[revision]).[ext]") }
-					metadataSources { artifact() }
-					content { includeModule("com.yarnpkg", "yarn") }
-				}
-			}
-			filter { includeGroup("com.yarnpkg") }
+		ivy("https://github.com/yarnpkg/yarn/releases/download") {
+			name = "Yarn Distributions at $url"
+			patternLayout { artifact("v[revision]/[artifact](-v[revision]).[ext]") }
+			metadataSources { artifact() }
+			content { includeModule("com.yarnpkg", "yarn") }
 		}
 
 		//region workaround for https://youtrack.jetbrains.com/issue/KT-51379
+		// Remove when Kotlin is updated to 2+
 		ivy("https://download.jetbrains.com/kotlin/native/builds") {
 			name = "Kotlin Native"
 			patternLayout {
